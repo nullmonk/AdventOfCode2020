dce2a076b1877cd8a324155c9fdd3e6d121957d6
micahjmartin <mjm5097@rit.edu>
2020-12-05 00:50:21 -0500

    condensed

--- 5/main.py
+++ 5/main.py
@@ -1,53 +1,6 @@
-import sys
-
-def getRowCol(string):
-    data = list(string[:-3])
-    x = 128
-    mn, mx = 1, 128
-    row, col = 0, 0
-    while data:
-        mid = (mx - mn)//2
-        let = data.pop(0)
-        if let == 'B':
-            mn = mx-mid
-        else:
-            mx = mn + mid
-        if mn == mx:
-            row = mn
-            break
-    data = list(string[-3:])
-    mn = 1
-    mx = 8
-    while data:
-        mid = (mx - mn)//2
-        let = data.pop(0)
-        if let == 'R':
-            mn = mx-mid
-        else:
-            mx = mn + mid
-        if mn == mx:
-            col = mn
-            break
-    return row-1, col-1, (row-1)*8+(col-1)
-
-
-seats = {}
-with open(sys.argv[1]) as fil:
-    ids = []
-    for line in fil.readlines():
-        line = line.strip()
-        if not line:
-            continue
-        seatdata = getRowCol(line)
-        seats[seatdata[2]] = seatdata[:2]
-        #ids += [seatdata[2]]
-    print(max(seats.keys()))
-
-# check the back row
-myseat = -1
-prev = False
-for i in range(0, 1028):
-    if i not in seats:
-        if i+1 in seats and i-1 in seats:
-            myseat = i
-print(myseat)
+seats = []
+for line in open("sample.txt").readlines():
+    line = line.strip().replace("F", "0").replace("B", '1').replace("L", "0").replace("R", "1")
+    seats.append((int(line[:-3], 2)*8)+int(line[-3:], 2))
+myseat = [i for i in range(1028) if i not in seats and i + 1 in seats and i - 1 in seats]
+print("Part 1: {}\nPart 2: {}".format(max(seats), myseat[0]))
