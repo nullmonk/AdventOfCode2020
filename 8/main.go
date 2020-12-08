decdc449bb728753ef1faa8d084d618d069fa266
micahjmartin <mjm5097@rit.edu>
2020-12-08 03:16:27 -0500

    cleanup

--- 8/main.go
+++ 8/main.go
@@ -1,103 +1,83 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
+	"io/ioutil"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func fatal(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-type programError struct {
-	typ int
-}
-
-func (e *programError) Error() string {
-	return fmt.Sprint("Program Error:", e.typ)
-}
-
-func (e *programError) Complete() bool {
-	if e.typ == 0 {
-		return true
-	}
-	return false
-}
-
-func (e *programError) Repeat() bool {
-	if e.typ == 1 {
-		return true
-	}
-	return false
-}
-
-func ProgramError(typ int) *programError {
-	return &programError{
-		typ: typ,
-	}
-}
-
+// State ...
 type State struct {
 	pc  int
 	acc int
-	ran map[int]bool // Instructions that have ran already
+	ran map[int]bool // Instructions that have run already
 }
 
+// Program ...
 type Program struct {
 	lines      []string
 	prevStates []*State
-	overRides  int
+	override   int // Override the instruction at this location
 	State
 }
 
-func (p *Program) Reset() {
-	p.lines = make([]string, 0)
+// Init the program
+func (p *Program) Init(filename string) {
+	//p.lines = make([]string, 0)
 	p.ran = make(map[int]bool)
-	p.overRides = -1
+	p.override = -1 // Very important
+
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	p.lines = strings.Split(string(data), "\n")
+	for p.lines[len(p.lines)-1] == "" {
+		p.lines = p.lines[:len(p.lines)-1]
+	}
+	//fmt.Printf("Loaded %v lines from %v\n", len(p.lines), filename)
 }
 
-func (p *Program) Next() *programError {
+func (p *Program) next() int {
 	line := p.lines[p.pc]
 	instr := strings.Split(line, " ")
 	val, _ := strconv.Atoi(instr[1])
 	if _, ok := p.ran[p.pc]; ok {
-		return ProgramError(1) // Repeated Instruction
+		return 1 // 1 indicates repeat
 	}
 
-	if p.pc == p.overRides {
-		p.Printf("Using override\n")
+	ovrstr := ""
+	if p.pc == p.override {
 		if instr[0] == "nop" {
 			instr[0] = "jmp"
 		} else {
 			instr[0] = "nop"
 		}
+		ovrstr = "*"
 	}
-	//fmt.Printf("%v: '%v'; %v %v\n", p.pc, line, instr[0], val)
 	switch instr[0] {
 	case "nop":
-		p.Printf("%v %v (saving state)\n", instr[0], val)
-		if p.overRides == -1 {
-			p.SaveState()
+		p.printf("%v%v %v\n", instr[0], ovrstr, val)
+		if p.override == -1 {
+			p.saveState()
 		}
 		p.ran[p.pc] = true
 		p.pc++
 		break
 	case "jmp":
-		p.Printf("%v %v (saving state)\n", instr[0], val)
-		if p.overRides == -1 {
-			p.SaveState()
+		p.printf("%v%v %v\n", ovrstr, instr[0], val)
+		if p.override == -1 {
+			p.saveState()
 		}
+
 		p.ran[p.pc] = true
 		p.pc += val
 		break
 	case "acc":
-		p.Printf("%v %v\n", instr[0], val)
+		p.printf("%v %v\n", instr[0], val)
 		p.acc += val
 		p.ran[p.pc] = true
 		p.pc++
@@ -105,26 +85,12 @@ func (p *Program) Next() *programError {
 	}
 
 	if p.pc >= len(p.lines) {
-		return ProgramError(0)
+		return 0 // 0 indicated program has completed
 	}
-	return ProgramError(-1)
+	return -1 // nO status currently. keep processing
 }
 
-func (p *Program) SwapInstruction(i int) {
-	line := p.lines[i]
-	p.Printf("Swapping inst. ")
-	if strings.Contains(line, "jmp") {
-		p.lines[i] = strings.Replace(line, "jmp", "nop", 1)
-		fmt.Println("jmp->nop")
-		return
-	}
-	if strings.Contains(line, "nop") {
-		p.lines[i] = strings.Replace(line, "nop", "jmp", 1)
-		fmt.Println("jmp->nop")
-	}
-}
-
-func (p *Program) SaveState() *State {
+func (p *Program) saveState() *State {
 	s := &State{
 		pc:  p.pc,
 		acc: p.acc,
@@ -134,16 +100,13 @@ func (p *Program) SaveState() *State {
 		s.ran[k] = v
 	}
 	p.prevStates = append(p.prevStates, s)
-	//p.Printf("Saving state\n")
+	p.printf("Saving state #%v\n", len(p.prevStates))
 	return s
 }
 
-func (p *Program) Load() {
+func (p *Program) load() {
 	i := len(p.prevStates) - 1
-	p.Printf("Hopping back a state: %v\n", i)
-	if i < 0 {
-		log.Fatal("Cannot go back any more")
-	}
+	p.printf("revert to state: #%v\n", i)
 	s := p.prevStates[i]
 	p.prevStates = p.prevStates[:i]
 	p.acc = s.acc
@@ -151,63 +114,36 @@ func (p *Program) Load() {
 	p.ran = s.ran
 }
 
-func (p *Program) LoadGiven(s *State) {
-	p.acc = s.acc
-	p.pc = s.pc
-	p.ran = s.ran
-}
-
-func (p *Program) AddLine(line string) {
-	p.lines = append(p.lines, line)
-}
-
-func (p *Program) RunP1() {
-	for true {
-		err := p.Next()
-		if err.Complete() || err.Repeat() {
-			fmt.Println(p.acc)
-			break
-		}
-	}
-}
-
+// Run the program and print the outputs
 func (p *Program) Run() {
+	p1Solved := false
 	for true {
-		err := p.Next()
-		if err.Complete() {
-			p.Printf("Program complete\n")
+		ret := p.next()
+		if ret == 0 {
+			p.printf("Program complete\n")
+			fmt.Printf("Part 2: %v\n", p.acc)
 			break
 		}
-		if err.Repeat() {
-			//fmt.Println("Repeated instruction: Jumping back in time")
-			p.Load()
-			p.overRides = p.pc
+		if ret == 1 {
+			if !p1Solved {
+				fmt.Printf("Part 1: %v\n", p.acc)
+				p1Solved = true
+			}
+			p.load()
+			p.override = p.pc
 		}
 	}
 }
 
-func (p *Program) Printf(s string, i ...interface{}) {
-	str := fmt.Sprintf(s, i...)
-	fmt.Printf("pc: %v acc: %v states: %v - %v", p.pc, p.acc, len(p.prevStates), str)
+func (p *Program) printf(s string, i ...interface{}) {
+	/*
+		str := fmt.Sprintf(s, i...)
+		fmt.Printf("pc: %v acc: %v states: %v - %v", p.pc, p.acc, len(p.prevStates), str)
+	*/
 }
 
 func main() {
-	file, err := os.Open(os.Args[1])
-	fatal(err)
-	defer file.Close()
-
 	program := &Program{}
-	program.Reset()
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := strings.Trim(scanner.Text(), " \n.")
-		program.AddLine(line)
-	}
-
-	/*start := program.SaveState()
-	program.RunP1()
-	program.LoadGiven(start)
-	program.prevStates = make([]*State, 0)
-	*/
+	program.Init(os.Args[1])
 	program.Run()
 }
